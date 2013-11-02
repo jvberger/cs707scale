4bb071d1f359e5c32fc6de5ca51e25794922c7cd
JohnCall <John.th.call@gmail.com>
2013-11-02 14:24:06 -0500

    Adds google maps fragment to second screen.
    https://developers.google.com/maps/documentation/android/start
    Adds three apks to install on emulator.
    Adds debug.keystore to set as keystore so api key works.

--- cs707scale/src/wisc/madison/cs/cs707scale/Map.java
+++ cs707scale/src/wisc/madison/cs/cs707scale/Map.java
@@ -2,22 +2,19 @@ package wisc.madison.cs.cs707scale;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.Menu;
 //import android.view.Menu;
 import android.widget.TextView;
 
-public class Map extends Activity {
+public class Map extends FragmentActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.map);
-		TextView display = (TextView)findViewById(R.id.text);
-		display.setText("Second Screen Text");
+		//TextView display = (TextView)findViewById(R.id.text);
+		//display.setText("Second Screen Text");
 	}
 
-	//@Override
-	//public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-	//	getMenuInflater().inflate(R.menu.scale, menu);
-	//	return true;
-	//}
+
 }
