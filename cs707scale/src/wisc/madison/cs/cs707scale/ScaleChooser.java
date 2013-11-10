35576686da954df8d210335d845f982bf42ac1cc
JohnCall <John.th.call@gmail.com>
2013-11-09 19:43:19 -0600

    Saves off scale directory lookup.
    The setting is remembered between application runs.

--- cs707scale/src/wisc/madison/cs/cs707scale/ScaleChooser.java
+++ cs707scale/src/wisc/madison/cs/cs707scale/ScaleChooser.java
@@ -1,8 +1,6 @@
 package wisc.madison.cs.cs707scale;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
@@ -13,12 +11,12 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
 import org.w3c.dom.Document;
-import org.w3c.dom.NodeList;
 
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
@@ -32,6 +30,7 @@ public class ScaleChooser extends Activity implements OnItemClickListener {
 	private ListView scales;
 	private ScaleChooser ref;
 	private String currentDirectory;
+	private static final String SETTINGSNAME = "ScaleSettings";
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -39,21 +38,11 @@ public class ScaleChooser extends Activity implements OnItemClickListener {
 		ref = this;
 		scales = (ListView)findViewById(R.id.listView1);
 		scales.setOnItemClickListener(this);
-		File file = new File(getExternalFilesDir(null), "Settings.xml");
-		if (file != null && file.exists())
+		SharedPreferences settings = getSharedPreferences(SETTINGSNAME, 0);
+		currentDirectory = settings.getString("scaleDirectory", "");
+		if (currentDirectory != "")
 		{
-			try {
-	            InputStream inputStream = new FileInputStream(file);
-	            DocumentBuilder docBuilder =  DocumentBuilderFactory.newInstance().newDocumentBuilder();
-	            Document doc = docBuilder.parse(inputStream);
-	            NodeList scaleLocation = doc.getElementsByTagName("scaleLocation");
-	            if (scaleLocation.getLength() != 0) {
-	            	currentDirectory = scaleLocation.item(0).getTextContent();
-	            	new LoadScalesTask().execute(currentDirectory);
-	            }
-			}
-			catch (Exception e) {
-			}
+			new LoadScalesTask().execute(currentDirectory);
 		}
 	}
 	
@@ -70,6 +59,11 @@ public class ScaleChooser extends Activity implements OnItemClickListener {
 		alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
 		public void onClick(DialogInterface dialog, int whichButton) {
 		  currentDirectory = input.getText().toString();
+	      SharedPreferences settings = getSharedPreferences(SETTINGSNAME, 0);
+	      SharedPreferences.Editor editor = settings.edit();
+	      editor.putString("scaleDirectory", currentDirectory);
+	      editor.commit();
+
 		  new LoadScalesTask().execute(currentDirectory);
 		  }
 		});
