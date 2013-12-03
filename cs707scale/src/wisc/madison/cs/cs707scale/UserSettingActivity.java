69c8d0a7c0270f8b927ce023b5ec6f06203afe82
jvberger <berger@cs.wisc.edu>
2013-12-02 18:15:39 -0600

    Updated a few interfaces. Added menu to map. Updated starting zoom on map.

---
+++ cs707scale/src/wisc/madison/cs/cs707scale/UserSettingActivity.java
@@ -0,0 +1,12 @@
+package wisc.madison.cs.cs707scale;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class UserSettingActivity extends PreferenceActivity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+}
